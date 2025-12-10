42fd46a8524175a7c30b58d381b0d18d6487c9d0
Keith <Karani.keith@student.greenriver.com>
2025-12-09 23:57:52 -0800

    made some changes to the index and added pages for lunch dinner and desserts

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("showSpecial").addEventListener("click", function () {
+    console.log("Special entree button clicked!");
+    document.getElementById("specialText").classList.remove("hidden");
+});
